fe45196825e193bdb1f394e0d4d9016ac32611a6
winspain <penndicardo@outlook.com>
2024-07-23 16:42:01 +0800

    feat install.sh

--- web/quick-install/quick-list.sh
+++ web/quick-install/quick-list.sh
@@ -5,7 +5,6 @@ set -e
 echo "clone repository..."
 git clone https://github.com/Winspain/aixian.git carlist
 
-
 # 设置目录名
 dir_name="list"
 # 检查目录是否存在
@@ -22,13 +21,15 @@ fi
 
 cd carlist/web
 mv dist/* ../../list
+mv custom/* ../../custom
 cd ../..
 chmod -R 755 list
 
 yaml_file="./docker-compose.yml"
 
 # 要检查和添加的映射
-check_volume="./list:/app/resource/public/list"
+check_volume_list="./list:/app/resource/public/list"
+check_volume_list_js="./custom/public/list.js:/app/resource/public/list.js"
 
 # 检查文件是否存在
 if [ ! -f "$yaml_file" ]; then
@@ -36,45 +37,44 @@ if [ ! -f "$yaml_file" ]; then
     exit 1
 fi
 
-# 检查是否已经存在该映射
-if grep -q "$check_volume" "$yaml_file"; then
-    echo "映射 '$check_volume' 已存在，无需添加。"
-    rm -rf carlist
-    echo "已完成前端页面的更换"
-    exit 0
-fi
+# 检查是否已经存在映射，并添加缺失的映射
+add_volume() {
+    local volume=$1
+    local new_volume=$2
+    if grep -q "$volume" "$yaml_file"; then
+        echo "映射 '$volume' 已存在，无需添加。"
+    else
+        awk -v new_volume="$new_volume" '
+        BEGIN {
+            in_chatgpt_share_server = 0;
+            in_volumes = 0;
+        }
+        /chatgpt-share-server:/ {
+            in_chatgpt_share_server = 1;
+        }
+        in_chatgpt_share_server && /volumes:/ {
+            in_volumes = 1;
+            print;
+            next;
+        }
+        in_volumes && /^[ ]+- / {
+            print new_volume;
+            in_volumes = 0;
+            in_chatgpt_share_server = 0;
+        }
+        { print }
+        ' docker-compose.yml > tmp_file && mv tmp_file docker-compose.yml
+        
+        if [ $? -eq 0 ]; then
+            echo "映射 '$new_volume' 添加成功"
+        else
+            echo "映射 '$volume' 添加失败"
+        fi
+    fi
+}
 
-# 定义要添加的新映射
-new_volume="      - ./list:/app/resource/public/list"
-
-# 使用 awk 更新 docker-compose.yml
-awk -v new_volume="$new_volume" '
-BEGIN {
-    in_chatgpt_share_server = 0;
-    in_volumes = 0;
-}
-/chatgpt-share-server:/ {
-    in_chatgpt_share_server = 1;
-}
-in_chatgpt_share_server && /volumes:/ {
-    in_volumes = 1;
-    print;
-    next;
-}
-in_volumes && /^[ ]+- / {
-    print new_volume;
-    in_volumes = 0;
-    in_chatgpt_share_server = 0;
-}
-{ print }
-' docker-compose.yml > tmp_file && mv tmp_file docker-compose.yml
-
-# 添加成功后运行 Docker 命令
-if [ $? -eq 0 ]; then
-    echo "映射 '$new_volume' 添加成功"
-else
-    echo "映射存在"
-fi
+add_volume "$check_volume_list" "      - $check_volume_list"
+add_volume "$check_volume_list_js" "      - $check_volume_list_js"
 
 rm -rf carlist
 docker compose pull
